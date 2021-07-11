520a974ab5a1e7ce8f3085b4a89602b7a5313b3d
stem league <64894084+stemleague@users.noreply.github.com>
2021-07-11 11:46:27 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+message = "hello world" # this is a testing message
+
+############### Encoding Process ###############
+# In this step we are going to encode a string to become a secret list.
+
+# Step 1: Reverse the message string
+
+# Step 2: Create a list called secret_list
+
+for letter in message:
+  # Step 3: Store the index of each letter from alphabet in a variable called letter_value
+
+  # Step 4: Add the letter_value to your secret_list
+
+# Step 5: Print secret list
+
+
+############### Decoding Process ###############
+# In this step we are going to decode the message in the secret list.
+
+# Step 6: Reverse secret_list
+
+# Step 7: Create a string called decoded_message
+
+for index in secret_list:
+  # Step 8: Find the character in the alphabet that corresponds to the index and add it to your decoded message
+
+# Step 9: Print your decoded message.
